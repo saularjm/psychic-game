ea094c512228b3e71087f58e79052465b66e17ae
Saular Moaddeli <jake.moaddeli@gmail.com>
2020-04-12 13:48:38 -0700

    first crack at game js code, w/o auto-reload

---
+++ assets/javascript/game.js
@@ -0,0 +1,50 @@
+// Array for computer options
+var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+// Variables for counters
+var wins = 0;
+var losses = 0;
+var guessCount = 9;
+var guesses = "";
+var count = 0;
+
+// Function for gameplay
+document.onkeyup = function(event) {
+
+    // User guess
+    var userGuess = event.key;
+
+    // Random computer choice
+    var comp = letters[Math.floor(Math.random() * letters.length)];
+
+    // Display results and increment counters
+    if (userGuess === comp) {
+        wins++;
+        document.getElementById("wins").innerHTML = wins;
+        guessCount = 9;
+        document.getElementById("guessCount").innerHTML = guessCount;
+        guesses = "";
+        document.getElementById("guesses").innerHTML = guesses;
+        // INSERT CODE HERE FOR RELOAD
+    }
+
+    else if (userGuess !== comp) {
+        if (count < 9) {
+            document.getElementById("guesses").innerHTML += userGuess;
+            guessCount--;
+            document.getElementById("guessCount").innerHTML = guessCount;
+            count++;
+        }
+        else if (count === 9) {
+            losses++;
+            document.getElementById("losses").innerHTML = losses;
+            guessCount = 9;
+            document.getElementById("guessCount").innerHTML = guessCount;
+            guesses = "";
+            document.getElementById("guesses").innerHTML = guesses;
+            // INSERT CODE HERE FOR RELOAD
+        }
+        
+    }
+
+}
