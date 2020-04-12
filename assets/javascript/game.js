fa4052e26b7319397179a8fd78210f8c69e75dad
Saular Moaddeli <jake.moaddeli@gmail.com>
2020-04-12 15:12:06 -0700

    commented JS, added Bootstrap style to HTML

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,17 +1,17 @@
 // Array for computer options
 var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-// Random computer choice
+// Random letter computer choice
 var comp = letters[Math.floor(Math.random() * letters.length)];
-console.log(comp);
 
-// Variables for counters
+// Variables for counters/outputs
 var wins = 0;
 var losses = 0;
 var guessCount = 9;
 var guesses = "";
 var count = 0;
 
+// Update HTML span tags to vars for starting values
 document.getElementById("wins").innerHTML = wins;
 document.getElementById("losses").innerHTML = losses;
 document.getElementById("guessCount").innerHTML = guessCount;
@@ -22,36 +22,50 @@ document.onkeyup = function(event) {
     // User guess
     var userGuess = event.key;
 
-    // Display results and increment counters
+    // For correct guesses
     if (userGuess === comp) {
+        // Increment wins and update HTML
         wins++;
         document.getElementById("wins").innerHTML = wins;
+        // Reset guesses remaining count and update HTML
         guessCount = 9;
         document.getElementById("guessCount").innerHTML = guessCount;
+        // Reset user guesses list and update HTML
         guesses = "";
         document.getElementById("guesses").innerHTML = guesses;
+        // Start counter over
         count = 0;
+        // Comp picks a new random letter
         comp = letters[Math.floor(Math.random() * letters.length)];
-        console.log(comp);
     }
 
+    // For wrong guesses
     else if (userGuess !== comp) {
+        // If user has guesses remaining
         if (count < 8) {
+            // Add user guess to guesses list and update HTML
             document.getElementById("guesses").innerHTML += userGuess + "  ";
+            // Decrement guesses remaining count and update HTML
             guessCount--;
             document.getElementById("guessCount").innerHTML = guessCount;
+            // Increment counter
             count++;
         }
+        // If user has used all their guesses
         else if (count === 8) {
+            // Increment losses and update HTML
             losses++;
             document.getElementById("losses").innerHTML = losses;
+            // Reset guesses remaining count and update HTML
             guessCount = 9;
             document.getElementById("guessCount").innerHTML = guessCount;
+            // Reset user guesses list and update HTML
             guesses = "";
             document.getElementById("guesses").innerHTML = guesses;
+            // Start counter over
             count = 0;
+            // Comp picks a new random letter
             comp = letters[Math.floor(Math.random() * letters.length)];
-            console.log(comp);
         }
         
     }
